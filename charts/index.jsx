ac3122a414a425f8d24d57476cbae00852620d33
maokl <maokl@Ctrip.com>
2018-06-02 00:12:13 +0800

    增加饼图

--- charts/index.jsx
+++ charts/index.jsx
@@ -26,17 +26,34 @@ class Charts extends React.Component {
     }
     
     componentDidMount() {
-        var myChart = echarts.init(document.getElementById('myChart'),'wonderland');
+        this.createBarChart();
+        this.createPieChart();
+    }
+    createBarChart = () => {
+        var myChart = echarts.init(document.getElementById('myChart'));
         const orderDates = [];
         const orderNums = [];
+        var yMax = 25;
+        var dataShadow = [];
+
+        
+
         request.getPromise(`http://localhost:8080/getOrderNumByDate`).then(json => {
         if (json) {
             for(let i = 0;i<json.length;i++){
                 orderDates.push(date.format(new Date(json[i].orderDate), 'yyyy-MM-dd'));
                 orderNums.push(json[i].orderNum);
             }
-
+            for (var i = 0; i < orderDates.length; i++) {
+                dataShadow.push(yMax);
+            }
             myChart.setOption({
+                tooltip : {
+                    trigger: 'axis',
+                    axisPointer : {
+                        type : 'line'
+                    }
+                },
                 title: {
                     text: '订单量统计'
                 },
@@ -45,23 +62,136 @@ class Charts extends React.Component {
                 },
                 tooltip: {},
                 xAxis: {
-                    data: orderDates || []
+                    data: orderDates || [],
+                    axisLabel: {
+                        inside: true,
+                        textStyle: {
+                            // color: '#fff'
+                        }
+                    },
+                    z: 10
                 },
-                yAxis: {},
-                series: [{
-                    name: '订单量',
-                    type: 'bar',
-                    data: orderNums || []
-                }]
+                yAxis: {
+                    axisLabel: {
+                        textStyle: {
+                            color: '#999'
+                        }
+                    }
+                },
+                series: [
+                    { // For shadow
+                        type: 'bar',
+                        itemStyle: {
+                            normal: {color: 'rgba(0,0,0,0.05)'}
+                        },
+                        barGap:'-100%',
+                        barCategoryGap:'40%',
+                        data: dataShadow,
+                        animation: false
+                    },
+                    {
+                        type: 'bar',
+                        markLine : {
+                            lineStyle: {
+                                normal: {
+                                    type: 'dashed'
+                                }
+                            },
+                            data : [
+                                [{type : 'min'}, {type : 'max'}]
+                            ]
+                        },
+                        itemStyle: {
+                            normal: {
+                                color: new echarts.graphic.LinearGradient(
+                                    0, 0, 0, 1,
+                                    [
+                                        {offset: 0, color: '#83bff6'},
+                                        {offset: 0.5, color: '#188df0'},
+                                        {offset: 1, color: '#188df0'}
+                                    ]
+                                )
+                            },
+                            emphasis: {
+                                color: new echarts.graphic.LinearGradient(
+                                    0, 0, 0, 1,
+                                    [
+                                        {offset: 0, color: '#2378f7'},
+                                        {offset: 0.7, color: '#2378f7'},
+                                        {offset: 1, color: '#83bff6'}
+                                    ]
+                                )
+                            }
+                        },
+                        data: orderNums || []
+                    }
+                ]
             });
         }
         }, error => {
             console.error('出错了', error);
         });
     }
+    createPieChart = () =>{
+        var pieChart = echarts.init(document.getElementById('pieChart'));
+        const orderDates = [];
+        const orderNums = [];
+        const data = [];
+
+        
+
+        request.getPromise(`http://localhost:8080/getOrderNumByDate`).then(json => {
+        if (json) {
+            for(let i = 0;i < 6;i++){
+                orderDates.push(date.format(new Date(json[i].orderDate), 'yyyy-MM-dd'));
+                data.push({'name': date.format(new Date(json[i].orderDate), 'yyyy-MM-dd') , 'value': json[i].orderNum});
+                console.log(orderDates)
+            }
+            pieChart.setOption(
+                { 
+                    title : {
+                    text: '订单分布',
+                    subtext: '',
+                    x:'center'
+                },
+                tooltip : {
+                    trigger: 'item',
+                    formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                    orient: 'vertical',
+                    left: 'left',
+                    data: orderDates
+                },
+                series : [
+                    {
+                        name: '日期',
+                        type: 'pie',
+                        radius : '55%',
+                        center: ['50%', '60%'],
+                        data: data,
+                        itemStyle: {
+                            emphasis: {
+                                shadowBlur: 10,
+                                shadowOffsetX: 0,
+                                shadowColor: 'rgba(0, 0, 0, 0.5)'
+                            }
+                        }
+                    }
+                ]
+            });
+        }
+        }, error => {
+            console.error('出错了', error);
+        });
+
+    }
     render() {
         return (
-            <div id="myChart" style={{ width: 1000, height: 400 }}></div>
+            <div>
+                <div id="myChart" style={{ width: 1200, height: 400 }}></div>
+                <div id="pieChart" style={{ width: 1200, height: 400, marginLeft: '200px' }}></div>
+            </div>
         );
     }
 }
